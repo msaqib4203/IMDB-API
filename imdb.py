df6845d840fb79ed77dad673492e9b2e3bd37795
Mohammad Saqib <msaqib4203@gmail.com>
2018-12-10 03:23:07 +0530

    Changes according to new changes in imdb

--- imdb.py
+++ imdb.py
@@ -3,47 +3,40 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+def parsePersons(persons):
+			
+	names = []
+	if isinstance(persons,dict):
+		names.append(persons['name'])
+		return names
+		
+	for person in persons:
+		if person['@type'] == "Person":
+			names.append(person['name'])
+	return names
 
 def getJSON(html):
-			
+
+	html_json =  html.find(attrs={'type':'application/ld+json'}).text.strip()
+	fetchedJson = json.loads(html_json)
 	data = {}
 	data['poster'] = html.find(attrs={'class':'poster'}).find('img')['src']
-	data['title'] =  html.find(itemprop='name').text.strip()
+	title_wrapper =  html.find(attrs={'class':'title_wrapper'}).text.strip()
+	data['title'] = title_wrapper[:title_wrapper.find(')')+1]
 	data['rating'] = html.find(itemprop='ratingValue').text
 	data['bestRating'] = html.find(itemprop='bestRating').text
 	data['votes'] = html.find(itemprop='ratingCount').text
-	data['rated'] = html.find(itemprop='contentRating')['content']
-	tags = html.findAll("span",{"itemprop":"genre"})
-	genres = []
-	for genre in tags:
-		genres.append(genre.text.strip())
-	data['genre'] = genres	
-		
-	data['description'] = html.find(itemprop="description").text.strip()
-
-	tags = html.findAll(itemprop="actors")
-	actors = []
-	for actor in tags:
-		actors.append(actor.text.strip().replace(',',''))
-	data['cast'] = actors	
-		
-
-	tags = html.findAll(itemprop="creator")
-	creators = []
-	for creator in tags:
-		creators.append(creator.text.strip().replace(',',''))
-	data['writers'] = creators	
-		
-	directors = []
-	tags = html.findAll(itemprop="director")
-	for director in tags:
-		directors.append(director.text.strip().replace(',',''))
-	data['directors'] = directors	
-		
+	data['rated'] = fetchedJson['contentRating']
+	data['genres'] = fetchedJson['genre']
+	data['description'] = fetchedJson['description']
+	data['cast'] = parsePersons(fetchedJson['actor'])
+	data['writers'] = parsePersons(fetchedJson['creator'])		
+	data['directors'] = parsePersons(fetchedJson['director'])	
 	json_data = json.dumps(data)
 	return json_data
 	
 def getHTML(url):
+	
 	response = requests.get(url)
 	return BeautifulSoup(response.content,'html.parser')	
 	
@@ -56,10 +49,11 @@ def getURL(input):
 			html = getHTML('https://www.google.co.in/search?q='+input)
 			for cite in html.findAll('cite'):
 				if 'imdb.com/title/tt' in cite.text:
-					html = getHTML('http://'+cite.text)
+					html = getHTML(cite.text)
 					break
 		return getJSON(html)	
 	except Exception as e:
+		print e
 		return 'Invalid input or Network Error!'
 		
 	
